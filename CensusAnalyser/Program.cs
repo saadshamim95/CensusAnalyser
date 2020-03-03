dba48bc4c02e7e31fd10afc493e1d0192f946d75
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-03-03 18:55:33 +0530

    [Saad] Add. First Commit

---
+++ CensusAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("*                 WELCOME TO CENSUS ANALYSER               *");
+            Console.WriteLine("************************************************************");
+        }
+    }
+}
